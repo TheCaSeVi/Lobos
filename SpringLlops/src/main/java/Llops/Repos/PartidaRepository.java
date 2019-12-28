5f57ff404754b617943a3de38bc44d8488253e1e
Carlos Serrano <caseov@gmail.com>
2019-12-28 18:00:12 +0100

    Proyecto limpio

--- SpringLlops/src/main/java/Llops/Repos/PartidaRepository.java
+++ SpringLlops/src/main/java/Llops/Repos/PartidaRepository.java
@@ -1,13 +1,9 @@
 package Llops.Repos;
 
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.data.repository.CrudRepository;
 
 import Llops.DataBase.*;
-import app.User;
 
-public interface PartidaRepository  extends CrudRepository<Partida, Integer> {
-	
+public interface PartidaRepository extends CrudRepository<Partida, Integer> {
+
 }
